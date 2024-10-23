090929fbdff00bfe6ba87d7cc2b14c3b66bfc6bc
Connor Lee <connojl2@uci.edu>
2024-10-22 18:09:59 -0700

    Add filtering for the right domains
    Filters right domains
    Stuck in calendar loop

--- scraper.py
+++ scraper.py
@@ -6,6 +6,13 @@ def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+'''
+    Need to check for:
+        traps/infinite loops
+        Possibly filter in the extract next links
+        Maybe shorten links in extract next link
+        
+'''
 def extract_next_links(url, resp):
     # Implementation required.
     # url: the URL that was used to get the page
@@ -16,39 +23,53 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    if resp.status != 200 or resp.raw_response.content == "":
+    if resp.status != 200 or not resp.raw_response.content or resp.raw_response.content == "":
         return ""
     soup = BeautifulSoup(resp.raw_response.content, "lxml")
     #for link in soup.find_all('a'):
     #    print(link.get('href'))
 
-    return [link.get('href') for link in soup.find_all('a')]
+    links = [link.get('href') for link in soup.find_all('a')]
+    processedLinks = []
+    for link in links:
+        if link: #Possible encoding error, so far only 12 occurences in the beginning
+            fragmentIndex = link.find("#")
+            if fragmentIndex >= 0:
+                processedLinks.append(link[0:fragmentIndex])
+            else:
+                processedLinks.append(link)
+    return processedLinks
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-
-    '''
-    Need to check for:
-        traps/infinite loops
-        Possibly filter in the extract next links
-        Maybe shorten links in extract next link
-        
-    '''
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+        '''print(parsed.netloc)
+        print(re.search(r"ics.uci.edu" 
+                    + r"|cs.uci.edu" 
+                    + r"|informatics.uci.edu"
+                    + r"|stat.uci.edu", parsed.netloc))'''
+        if (re.search(r"ics.uci.edu" 
+                    + r"|cs.uci.edu" 
+                    + r"|informatics.uci.edu"
+                    + r"|stat.uci.edu", parsed.netloc)
+            or re.match(r"today.uci.edu/department/information_computer_sciences", parsed.netloc)):
+            return not re.match(
+                r".*\.(css|js|bmp|gif|jpe?g|ico"
+                + r"|png|tiff?|mid|mp2|mp3|mp4"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                + r"|epub|dll|cnf|tgz|sha1"
+                + r"|thmx|mso|arff|rtf|jar|csv"
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+        return False
 
     except TypeError:
         print ("TypeError for ", parsed)
