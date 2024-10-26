63c001ddfaa862b83453737627e22798e92d0754
ALVIS YONG <amyong@uci.edu>
2024-10-26 16:06:39 -0700

    Cleanup for store_url

--- utils/store_url.py
+++ utils/store_url.py
@@ -6,28 +6,3 @@ def store_url(url, resp):
 
 def compress(url, resp):
     return 
-
-    # Avoid Random Character Sequences
-    if re.search(r'/[a-zA-Z0-9]{10,}/', url):
-        return False
-
-    # Avoid Excessive Length
-    if len(url) > 200:
-        return False
-
-    # Avoid Excessive Query Parameters
-    parsed = urlparse(url)
-    if len(parse_qs(parsed.query)) > 5:
-        return False
-
-    # Avoid Repeated URL Path Segments
-    path_segments = parsed.path.split('/')
-    if len(set(path_segments)) < len(path_segments):
-        return False
-
-    # Avoid Specific Keywords Common in Traps
-    trap_keywords = ['empty', 'trap', 'hidden']
-    if any(trap_word in url.lower() for trap_word in trap_keywords):
-        return False
-    
-    return True
