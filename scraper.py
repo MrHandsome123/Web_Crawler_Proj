a31af766a2ff9fc1321d0855104fb0c091d0652f
zhanhaoruan <zhanhaor@uci.edu>
2024-10-27 11:49:47 -0700

    Implement extract_next_links function

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,19 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    result = []
+
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    hyperlinks = [a['href'] for a in soup.find_all('a', href=True)]
+
+    for link in hyperlinks:
+        if not link.startswith('http'):
+            continue
+        result.append(urljoin(url, link))
+        
+
+    return result
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
