8b06ada20dd3389938d318fc33f41244a8443f5c
Mr_Handsome123 <12011408@mail.sustech.edu.cn>
2025-02-01 12:45:36 -0800

    update config.ini

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,11 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    links = []
+    try:
+        if resp.staus == 200:
+
+    return links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
